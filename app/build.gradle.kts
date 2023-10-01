c652f433de322f2bc1e55cd3db67418776f9ae5f
anujsaxenadev <anuj.saxena.dev@gmail.com>
2023-10-01 19:26:57 +0530

    icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("com.airbnb.android:lottie-compose:6.1.0")
     implementation("io.github.sridhar-sp:neumorphic:0.0.6")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0-alpha06")
 
 }
 
