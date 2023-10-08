a2781986be678677693094f5803d50f0af3b0ea9
anujsaxenadev <anuj.saxena.dev@gmail.com>
2023-10-08 19:49:39 +0530

    animated border

---
+++ app/src/main/java/com/wordpress/anujsaxenadev/audiorecorder/audio_list/components/AnimatedBorderCard.kt
@@ -0,0 +1,58 @@
+package com.wordpress.anujsaxenadev.audiorecorder.audio_list.components
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
+import com.wordpress.anujsaxenadev.audiorecorder.ui.theme.Dimen_4adp
+
+@Composable
+fun AnimatedBorderCard(
+    modifier: Modifier,
+    shape: RoundedCornerShape,
+    content: @Composable () -> Unit
+){
+    val duration = 10000
+    val infiniteTransition = rememberInfiniteTransition("AnimatedBorderCardAnimation")
+    val degrees = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = duration, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = ""
+    ).value
+
+    Surface(
+        modifier = modifier,
+        shape = shape
+    ) {
+        Surface(modifier = Modifier
+            .padding(Dimen_4adp)
+            .drawWithContent {
+                rotate(degrees = degrees) {
+                    drawCircle(
+                        brush = Brush.linearGradient(listOf(Color.Magenta, Color.Cyan)),
+                        radius = size.width,
+                        blendMode = BlendMode.SrcIn
+                    )
+                }
+                drawContent()
+            }) {
+            content()
+        }
+    }
+}
--- app/src/main/java/com/wordpress/anujsaxenadev/audiorecorder/core/components/AudioImageTile.kt
+++ app/src/main/java/com/wordpress/anujsaxenadev/audiorecorder/core/components/AudioImageTile.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import com.gandiva.neumorphic.LightSource
 import com.gandiva.neumorphic.neu
+import com.wordpress.anujsaxenadev.audiorecorder.audio_list.components.AnimatedBorderCard
 import com.wordpress.anujsaxenadev.audiorecorder.ui.theme.DSGradientEnd
 import com.wordpress.anujsaxenadev.audiorecorder.ui.theme.DSGradientStart
 import com.wordpress.anujsaxenadev.audiorecorder.ui.theme.DSWhite
@@ -21,11 +22,8 @@ fun AudioImageTile(
     id: Int,
     audioImageTileType: AudioImageTileType
 ){
-    val roundedRadius = Dimen_10adp
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = DSWhite
-        ),
+    val shape = RoundedCornerShape(Dimen_10adp)
+    AnimatedBorderCard(
         modifier = Modifier
             .fillMaxWidth(0.8f)
             .neu(
@@ -34,24 +32,32 @@ fun AudioImageTile(
                 shadowElevation = Dimen_50adp,
                 lightSource = LightSource.LEFT_TOP,
             ),
-        shape = RoundedCornerShape(roundedRadius),
+        shape = shape,
     ){
-        val modifier = Modifier
-            .aspectRatio(1f)
-            .clip(RoundedCornerShape(roundedRadius))
-        when(audioImageTileType){
-            AudioImageTileType.AnimatedImageJSON -> {
-                AnimatedImageView.AnimatedImage(
-                    id = id,
-                    modifier = modifier)
-            }
-            AudioImageTileType.LocalImage -> {
-                LocalImageView.getLocalImage(
-                    id,
-                    modifier = modifier)
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = DSWhite
+            ),
+            shape = shape
+        ){
+            val modifier = Modifier
+                .aspectRatio(1f)
+                .clip(shape)
+            when(audioImageTileType){
+                AudioImageTileType.AnimatedImageJSON -> {
+                    AnimatedImageView.AnimatedImage(
+                        id = id,
+                        modifier = modifier)
+                }
+                AudioImageTileType.LocalImage -> {
+                    LocalImageView.getLocalImage(
+                        id,
+                        modifier = modifier)
+                }
             }
         }
     }
+
 }
 
 sealed interface AudioImageTileType{
